f4b1fd7b0b09acc6bac09c07230a3d523b5d2d21
Michel Richard <mrichard4@lm3000026417.home>
2014-10-12 17:50:39 -0400

    change app start to express 4

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.route( '/heartbeat' )
 });
 
 // create server
-http.createServer( app ).listen( app.get('port') );
+app.listen( app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
+});
 
 module.exports = app;
