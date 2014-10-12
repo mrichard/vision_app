33e72f585783c7115336f6e08dd812fc6eb628a9
Michel Richard <mrichard4@lm3000026417.home>
2014-10-12 19:20:16 -0400

    split heartbeat routes

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 var express = require( 'express' );
 var http = require( 'http' );
 var config = require( './config/config');
+var heartbeat = require( './routes/heartbeat' );
+
 var app = express();
 
 // set port
 app.set( 'port', config.get( "express:port" ) );
 
 // create heatbeat route
-app.route( '/heartbeat' )
-.get( function( req, res ){
-	res.status( 200 ).json( 'OK' );
-});
+app.use( '/heartbeat', heartbeat );
 
 // create server
 app.listen( app.get('port'), function() {
